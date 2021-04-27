3232656ebbacec3482a66a30687e0b55934078c6
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-04-27 12:43:06 -0700

    added some tests

--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -32,9 +32,7 @@ function Nav() {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
-                            About me
-                        </a>
+                        <a href="#about">About me</a>
                     </li>
                     <li>
                         <span>Contact</span>
