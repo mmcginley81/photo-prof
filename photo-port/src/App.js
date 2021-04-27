053823d90e491cc97f8eca8263494a8c9e67cc0f
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-04-26 17:04:31 -0700

    added nav and about me

---
+++ photo-port/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import About from './components/About'
+//import logo from './logo.svg';
+import Nav from './components/Nav';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+        </main>
+    </div>
+  );
+}
+
+export default App;
